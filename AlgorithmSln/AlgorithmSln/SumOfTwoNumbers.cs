4189d95259dcd8be888701bd45b7e86c2ba49fc4
dvbb <578760600@qq.com>
2021-07-05 22:42:52 +0800

    update sum of two numbers

---
+++ AlgorithmSln/AlgorithmSln/SumOfTwoNumbers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmSln
+{
+    public class SumOfTwoNumbers
+    {
+        //intpu：nums = [2,7,11,15], target = 9
+        //output：[0,1]
+        //because： nums[0] + nums[1] == 9 ，return [0, 1]
+
+        public int[] TwoSum(int[] nums, int target)
+        {
+            int[] result = new int[2];
+            Hashtable ht = new Hashtable();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                ht.Add(nums[i],i);
+            }
+            int diff;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                diff = target - nums[i];
+                if (ht.Contains(diff))
+                {
+                    result[0] = (int)ht[diff];
+                    result[1] = i;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ AlgorithmSln/Tests/SumOfTwoNumbersTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AlgorithmSln;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Common;
+
+namespace Tests
+{
+    [TestClass()]
+    public class SumOfTwoNumbersTests
+    {
+        public SumOfTwoNumbers member = new SumOfTwoNumbers();
+
+        [TestMethod()]
+        public void TwoSumTest()
+        {
+            int[] nums1 = { 2, 7, 11, 15 };
+            int[] nums2 = { 3, 2, 4 };
+            int[] nums3 = { 3, 3 };
+            string expected1 = "[0,1]";
+            string expected2 = "[1,2]";
+            string expected3 = "[0,1]";
+            Assert.IsTrue(MyFormat.Format(member.TwoSum(nums1, 9)) == expected1);
+            Assert.IsTrue(MyFormat.Format(member.TwoSum(nums2, 6)) == expected2);
+            //Assert.IsTrue(MyFormat.Format(member.TwoSum(nums3, 6)) == expected3);
+        }
+    }
+}
