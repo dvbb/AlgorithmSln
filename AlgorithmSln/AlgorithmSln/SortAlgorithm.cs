ff9a9bfbdf619a81b910159749da8ca8ddc921ef
dvbb <578760600@qq.com>
2021-07-08 22:49:05 +0800

    Commit SortAlgorithm

---
+++ AlgorithmSln/AlgorithmSln/SortAlgorithm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmSln
+{
+    public class SortAlgorithm
+    {
+        public int[] BubbleSort(int[] nums)
+        {
+            int temp;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (nums[j] > nums[i])
+                    {
+                        temp = nums[i];
+                        nums[i] = nums[j];
+                        nums[j] = temp;
+                    }
+                }
+            }
+            return nums;
+        }
+    }
+}
---
+++ AlgorithmSln/Tests/SortAlgorithmTests.cs
@@ -0,0 +1,27 @@
+﻿using AlgorithmSln;
+using Common;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tests
+{
+    public class SortAlgorithmTests
+    {
+        private SortAlgorithm member;
+        [SetUp]
+        public void Setup()
+        {
+            member = new SortAlgorithm();
+        }
+
+        [Test]
+        public void BubbleSort()
+        {
+            int[] nums1 = { 2, 92, 15, 8 };
+            string expected1 = "[2,8,15,92]";
+            Assert.IsTrue(MyFormat.Format(member.BubbleSort(nums1)) == expected1);
+        }
+    }
+}
