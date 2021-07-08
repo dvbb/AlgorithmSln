585fc6b6db000d75d5f9d24ca4965304b22992f6
dvbb <578760600@qq.com>
2021-07-08 22:36:25 +0800

    update SumOfTwoNum

--- AlgorithmSln/AlgorithmSln/SumOfTwoNumbers.cs
+++ AlgorithmSln/AlgorithmSln/SumOfTwoNumbers.cs
@@ -10,14 +10,24 @@ namespace AlgorithmSln
         //intpu：nums = [2,7,11,15], target = 9
         //output：[0,1]
         //because： nums[0] + nums[1] == 9 ，return [0, 1]
-
         public int[] TwoSum(int[] nums, int target)
         {
             int[] result = new int[2];
             Hashtable ht = new Hashtable();
             for (int i = 0; i < nums.Length; i++)
             {
-                ht.Add(nums[i],i);
+                if (!ht.ContainsKey(nums[i]))
+                {
+                    ht.Add(nums[i], i);
+                    continue;
+                }
+                //if hashtable contain nums[i],check nums[i] + nums[i] == target is true,return the result
+                if (nums[i] + nums[i] == target)
+                {
+                    result[0] = (int)ht[nums[i]];
+                    result[1] = i;
+                    return result;
+                }
             }
             int diff;
             for (int i = 0; i < nums.Length; i++)
--- AlgorithmSln/Tests/SumOfTwoNumbersTests.cs
+++ AlgorithmSln/Tests/SumOfTwoNumbersTests.cs
@@ -1,29 +1,36 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using AlgorithmSln;
+﻿using AlgorithmSln;
 using System;
 using System.Collections.Generic;
 using System.Text;
 using Common;
-
+using NUnit.Framework;
 namespace Tests
 {
-    [TestClass()]
     public class SumOfTwoNumbersTests
     {
-        public SumOfTwoNumbers member = new SumOfTwoNumbers();
+        private  SumOfTwoNumbers member;
 
-        [TestMethod()]
+        [SetUp]
+        public void Setup()
+        {
+            member = new SumOfTwoNumbers();
+        }
+
+        [Test]
         public void TwoSumTest()
         {
             int[] nums1 = { 2, 7, 11, 15 };
             int[] nums2 = { 3, 2, 4 };
             int[] nums3 = { 3, 3 };
+            int[] nums4 = { 3, 5,5,3 };
             string expected1 = "[0,1]";
             string expected2 = "[1,2]";
             string expected3 = "[0,1]";
+            string expected4 = "[0,3]";
             Assert.IsTrue(MyFormat.Format(member.TwoSum(nums1, 9)) == expected1);
             Assert.IsTrue(MyFormat.Format(member.TwoSum(nums2, 6)) == expected2);
-            //Assert.IsTrue(MyFormat.Format(member.TwoSum(nums3, 6)) == expected3);
+            Assert.IsTrue(MyFormat.Format(member.TwoSum(nums3, 6)) == expected3);
+            Assert.IsTrue(MyFormat.Format(member.TwoSum(nums4, 6)) == expected4);
         }
     }
 }
