5ac66332ae8affbe6d3d7bea38cbfb0788eb4892
Tier Ruan <578760600@qq.com>
2021-07-09 22:53:32 +0800

    Update Sort Algorithm
    Update Sort Algorithm

--- AlgorithmSln/AlgorithmSln/SortAlgorithm.cs
+++ AlgorithmSln/AlgorithmSln/SortAlgorithm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Common;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -21,6 +22,67 @@ namespace AlgorithmSln
                     }
                 }
             }
+            return nums;
+        }
+
+        //Each cycle select the smallest item , and place it at the nums[i]
+        public int[] SelectionSort(int[] nums)
+        {
+            int min,count;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                min = nums[i];
+                count = i;
+                for (int j = i; j < nums.Length; j++)
+                {
+                    if (nums[j]<min)
+                    {
+                        count = j;
+                        min = nums[j];
+                    }
+                }
+                Utilities.Swap(ref nums[i], ref nums[count]);
+            }
+            return nums;
+        }
+
+        public int[] InsertionSort(int[] nums)
+        {
+            return nums;
+        }
+
+        public int[] ShellSort(int[] nums)
+        {
+            return nums;
+        }
+
+        public int[] MergeSort(int[] nums)
+        {
+            return nums;
+        }
+
+        public int[] QuickSort(int[] nums)
+        {
+            return nums;
+        }
+
+        public int[] HeapSort(int[] nums)
+        {
+            return nums;
+        }
+
+        public int[] CountingSort(int[] nums)
+        {
+            return nums;
+        }
+
+        public int[] BucketSort(int[] nums)
+        {
+            return nums;
+        }
+
+        public int[] RadixSort(int[] nums)
+        {
             return nums;
         }
     }
---
+++ AlgorithmSln/Common/Utilities.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common
+{
+    public static class Utilities
+    {
+        public static void  Swap(ref int num1,ref int num2)
+        {
+            int temp = num1;
+            num1 = num2;
+            num2 = temp;
+        }
+    }
+}
