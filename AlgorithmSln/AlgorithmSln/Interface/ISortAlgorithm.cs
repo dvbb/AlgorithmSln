e93b03a4f400a520f391d8c96cc7dd127041ae31
Tier Ruan <578760600@qq.com>
2021-07-10 09:45:33 +0800

    Add Interface definition
    Add Interface definition

---
+++ AlgorithmSln/AlgorithmSln/Interface/ISortAlgorithm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmSln.Interface
+{
+    public interface ISortAlgorithm
+    {
+        int[] BubbleSort(int[] nums);
+        int[] SelectionSort(int[] nums);
+        int[] InsertionSort(int[] nums);
+        int[] ShellSort(int[] nums);
+        int[] MergeSort(int[] nums);
+        int[] QuickSort(int[] nums);
+        int[] HeapSort(int[] nums);
+        int[] CountingSort(int[] nums);
+        int[] BucketSort(int[] nums);
+        int[] RadixSort(int[] nums);
+    }
+}
---
+++ AlgorithmSln/AlgorithmSln/Interface/ISumOfTwoNumbers.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmSln.Interface
+{
+    public interface ISumOfTwoNumbers
+    {
+         int[] TwoSum(int[] nums, int target);
+    }
+}
