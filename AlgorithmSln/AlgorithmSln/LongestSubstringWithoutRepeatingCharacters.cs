a7d6bff20a11435a0ff75ce704f543590385672c
dvbb <61542202+dvbb@users.noreply.github.com>
2021-09-01 14:58:32 +0800

    add 3. Longest Substring Without Repeating Characters

---
+++ AlgorithmSln/AlgorithmSln/LongestSubstringWithoutRepeatingCharacters.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmSln
+{
+    public class LongestSubstringWithoutRepeatingCharacters
+    {
+        // 3. Longest Substring Without Repeating Characters
+        // Given a string s, find the length of the longest substring without repeating characters.
+
+        //Input: s = "abcabcbb"
+        //Output: 3
+        //Explanation: The answer is "abc", with the length of 3.
+        public int LengthOfLongestSubstring(string s)
+        {
+            int result = 0;
+            int count = 0;
+            int cur = 0;
+            string substr = "";
+            HashSet<char> hashSet = new HashSet<char>();
+            //time cost too much, need optimization in next step
+            while (s.Length > cur + result)
+            {
+                if (substr.Length == s.Length)
+                {
+                    result = substr.Length;
+                    break;
+                }
+                if (cur + substr.Length >= s.Length)
+                {
+                    result = (result > substr.Length) ? result : substr.Length;
+                    break;
+                }
+                if (!hashSet.Contains(s[count]))
+                {
+                    substr += s[count];
+                    hashSet.Add(s[count]);
+                    count++;
+                }
+                else
+                {
+                    result = (result > substr.Length) ? result : substr.Length;
+                    cur++;
+                    count = cur;
+                    hashSet.Clear();
+                    substr = "";
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ AlgorithmSln/Tests/LongestSubstringWithoutRepeatingCharactersTests.cs
@@ -0,0 +1,41 @@
+﻿using AlgorithmSln;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmTests
+{
+    public class LongestSubstringWithoutRepeatingCharactersTests
+    {
+        private LongestSubstringWithoutRepeatingCharacters _member;
+        [SetUp]
+        public void Setup()
+        {
+            _member = new LongestSubstringWithoutRepeatingCharacters();
+        }
+
+        [Test]
+        public void LongestSubstringWithoutRepeatingCharactersTest()
+        {
+            string str1 = "abcabcbb";
+            string str2 = "bbbbb";
+            string str3 = "pwwkew";
+            string str4 = "";
+            string str5 = "abv";
+            string str6 = "aab";
+            int expected1 = 3;
+            int expected2 = 1;
+            int expected3 = 3;
+            int expected4 = 0;
+            int expected5 = 3;
+            int expected6 = 2;
+            Assert.AreEqual(_member.LengthOfLongestSubstring(str1), expected1);
+            Assert.AreEqual(_member.LengthOfLongestSubstring(str2), expected2);
+            Assert.AreEqual(_member.LengthOfLongestSubstring(str3), expected3);
+            Assert.AreEqual(_member.LengthOfLongestSubstring(str4), expected4);
+            Assert.AreEqual(_member.LengthOfLongestSubstring(str5), expected5);
+            Assert.AreEqual(_member.LengthOfLongestSubstring(str6), expected6);
+        }
+    }
+}
