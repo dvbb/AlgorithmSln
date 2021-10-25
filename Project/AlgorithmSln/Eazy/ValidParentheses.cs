cb516e7886986e221da514d069eed68c8f6acb7f
Chengming Ruan <v-cruan@microsoft.com>
2021-10-25 15:29:34 +0800

    Add  20.Valid Parentheses(Simple)

---
+++ Project/AlgorithmSln/Eazy/ValidParentheses.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.Eazy
+{
+    public class ValidParentheses
+    {
+        /// <summary>
+        /// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+        /// An input string is valid if:
+        /// 1.Open brackets must be closed by the same type of brackets.
+        /// 2.Open brackets must be closed in the correct order.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public bool IsValid_ByStack(string str)
+        {
+            if (str.Length % 2 != 0)
+            {
+                return false;
+            }
+            Stack<char> stack = new Stack<char>();
+            foreach (char ch in str)
+            {
+                if (ch is '(' || ch is '[' || ch is '{')
+                {
+                    stack.Push(ch);
+                    continue;
+                }
+                if ((ch is ')' || ch is ']' || ch is '}') && stack.Count == 0)
+                {
+                    return false;
+                }
+                if (ch is ')')
+                {
+                    if (stack.Pop() != '(')
+                    {
+                        return false;
+                    }
+                }
+                if (ch is ']')
+                {
+                    if (stack.Pop() != '[')
+                    {
+                        return false;
+                    }
+                }
+                if (ch is '}')
+                {
+                    if (stack.Pop() != '{')
+                    {
+                        return false;
+                    }
+                }
+            }
+            if (stack.Count == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// time cost and space cost are close to V1.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public bool IsValid_ByStackV2(string s)
+        {
+            Dictionary<char, char> dic = new Dictionary<char, char>
+            {
+                { ']', '[' },
+                { ')', '(' },
+                { '}', '{' },
+            };
+            var stack = new Stack<char>();
+            foreach (var c in s)
+            {
+                if (dic.ContainsKey(c))
+                {
+                    if (stack.Count == 0) return false;
+                    if (stack.Pop() != dic[c]) return false;
+                }
+                else
+                    stack.Push(c);
+            }
+            return stack.Count == 0 ? true : false;
+        }
+
+        /// <summary>
+        /// time cost and space cost is highest
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public bool IsValid_ByReplace(string s)
+        {
+            while(s.Contains("()")|| s.Contains("[]")|| s.Contains("{}"))
+            {
+                s = s.Replace("()", "").Replace("[]", "").Replace("{}", "");
+            }
+            if (s is "")
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Project/Tests/Easy/ValidParenthesesTests.cs
@@ -0,0 +1,82 @@
+﻿using Algorithm.Eazy;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tests.Easy
+{
+    class ValidParenthesesTests
+    {
+        private ValidParentheses _member;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _member = new ValidParentheses();
+        }
+
+        [Test]
+        public void IsValid_ByStack()
+        {
+            string input1 = "()";
+            string input2 = "()[]{}";
+            string input3 = "(]";
+            string input4 = "([)]";
+            string input5 = "{[]}";
+            string input6 = "((";
+            string input7 = "))";
+            string input8 = "";
+            Assert.AreEqual(true, _member.IsValid_ByStack(input1));
+            Assert.AreEqual(true, _member.IsValid_ByStack(input2));
+            Assert.AreEqual(false, _member.IsValid_ByStack(input3));
+            Assert.AreEqual(false, _member.IsValid_ByStack(input4));
+            Assert.AreEqual(true, _member.IsValid_ByStack(input5));
+            Assert.AreEqual(false, _member.IsValid_ByStack(input6));
+            Assert.AreEqual(false, _member.IsValid_ByStack(input7));
+            Assert.AreEqual(true, _member.IsValid_ByStack(input8));
+        }
+
+        [Test]
+        public void IsValid_ByStackV2()
+        {
+            string input1 = "()";
+            string input2 = "()[]{}";
+            string input3 = "(]";
+            string input4 = "([)]";
+            string input5 = "{[]}";
+            string input6 = "((";
+            string input7 = "))";
+            string input8 = "";
+            Assert.AreEqual(true, _member.IsValid_ByStackV2(input1));
+            Assert.AreEqual(true, _member.IsValid_ByStackV2(input2));
+            Assert.AreEqual(false, _member.IsValid_ByStackV2(input3));
+            Assert.AreEqual(false, _member.IsValid_ByStackV2(input4));
+            Assert.AreEqual(true, _member.IsValid_ByStackV2(input5));
+            Assert.AreEqual(false, _member.IsValid_ByStackV2(input6));
+            Assert.AreEqual(false, _member.IsValid_ByStackV2(input7));
+            Assert.AreEqual(true, _member.IsValid_ByStackV2(input8));
+        }
+
+        [Test]
+        public void IsValid_ByReplace()
+        {
+            string input1 = "()";
+            string input2 = "()[]{}";
+            string input3 = "(]";
+            string input4 = "([)]";
+            string input5 = "{[]}";
+            string input6 = "((";
+            string input7 = "))";
+            string input8 = "";
+            Assert.AreEqual(true, _member.IsValid_ByReplace(input1));
+            Assert.AreEqual(true, _member.IsValid_ByReplace(input2));
+            Assert.AreEqual(false, _member.IsValid_ByReplace(input3));
+            Assert.AreEqual(false, _member.IsValid_ByReplace(input4));
+            Assert.AreEqual(true, _member.IsValid_ByReplace(input5));
+            Assert.AreEqual(false, _member.IsValid_ByReplace(input6));
+            Assert.AreEqual(false, _member.IsValid_ByReplace(input7));
+            Assert.AreEqual(true, _member.IsValid_ByReplace(input8));
+        }
+    }
+}
