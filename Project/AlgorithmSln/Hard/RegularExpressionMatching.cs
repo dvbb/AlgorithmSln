e8fbcb84b1a44852b5557e90960ea1d389b66a3e
Chengming Ruan <v-cruan@microsoft.com>
2021-10-26 16:47:39 +0800

    Add 10.Regular Expression Matching

---
+++ Project/AlgorithmSln/Hard/RegularExpressionMatching.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.Hard
+{
+    public class RegularExpressionMatching
+    {
+
+        /// <summary>
+        /// Given an input string s and a pattern p, implement regular expression matching with support for '.' and '*' where:
+        /// '.' Matches any single character.​​​​
+        /// '*' Matches zero or more of the preceding element.
+        /// The matching should cover the entire input string (not partial).
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="p"></param>
+        /// <returns></returns>
+        public bool IsMatch(string s, string p)
+        {
+            if (p is ".*")
+            {
+                return true;
+            }
+            string s5 = "adfsbxbcdacfsc", p5 = "a.*bc.*c";
+            var list = p5.Split(".*");
+            int j = 0;
+            bool result = true;
+            for (int i = 0; i < s.Length; i++)
+            {
+                result = Compare(s, list, ref i, ref j);
+                if (j > list.Length)
+                {
+                    return true;
+                }
+                if (!result)
+                {
+                    return result;
+                }
+            }
+            return result;
+        }
+
+        private static bool Compare(string s, string[] list, ref int i, ref int j)
+        {
+            for (int x = 0; x < list[j].Length; x++)
+            {
+                if (s[i] == list[j][x])
+                {
+                    i++;
+                }
+            }
+            j++;
+            return true;
+        }
+    }
+}
---
+++ Project/Tests/Hard/RegularExpressionMatchingTests.cs
@@ -0,0 +1,54 @@
+﻿using Algorithm.Hard;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlorithmTests.Hard
+{
+    public class RegularExpressionMatchingTests
+    {
+        RegularExpressionMatching _member;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _member = new RegularExpressionMatching(); ;
+        }
+
+        /// <summary>
+        /// 1 <= s.length <= 20
+        /// 1 <= p.length <= 30
+        /// s contains only lowercase English letters.
+        /// p contains only lowercase English letters, '.', and '*'.
+        /// It is guaranteed for each appearance of the character '*', there will be a previous valid character to match.
+        /// </summary>
+        [Test]
+        [Ignore("wait for coding")]
+        public void IsMatch()
+        {
+            string s1 = "aa", p1 = "a";
+            string s2 = "aa", p2 = "a*";
+            string s3 = "ab", p3 = ".*";
+            string s4 = "aab", p4 = "c*a*b";
+            string s5 = "mississippi", p5 = "mis*is*p*.";
+            string s6 = "", p6 = "";
+            string s7 = "sjqsafsfsazz", p7 = "sjq.*zz.";
+            string s8 = "sjqsafsfsazz", p8 = ".*zz.";
+            string s9 = "sjqsafsfsazz", p9 = "sjq.*";
+            string s10 = "adfsbxdacfsc", p10 = "a.*b.*c";
+            string s11 = "adfsbxbcdacfsc", p11 = "a.*bc.*c";
+            Assert.AreEqual(false, _member.IsMatch(s1, p1));
+            Assert.AreEqual(true, _member.IsMatch(s2, p2));
+            Assert.AreEqual(true, _member.IsMatch(s3, p3));
+            Assert.AreEqual(true, _member.IsMatch(s4, p4));
+            Assert.AreEqual(false, _member.IsMatch(s5, p5));
+            Assert.AreEqual(true, _member.IsMatch(s6, p6));
+            Assert.AreEqual(true, _member.IsMatch(s7, p7));
+            Assert.AreEqual(true, _member.IsMatch(s8, p8));
+            Assert.AreEqual(true, _member.IsMatch(s9, p9));
+            Assert.AreEqual(true, _member.IsMatch(s10, p10));
+            Assert.AreEqual(true, _member.IsMatch(s11, p11));
+        }
+    }
+}
