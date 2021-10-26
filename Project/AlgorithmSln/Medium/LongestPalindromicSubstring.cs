cc7c4831eee1176e32eb8cc3255f0ef8324c4955
Chengming Ruan <v-cruan@microsoft.com>
2021-10-26 09:50:45 +0800

    Add LongestPalindrome(Medium)
    use ExpandAroundCenter

---
+++ Project/AlgorithmSln/Medium/LongestPalindromicSubstring.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.Medium
+{
+    public class LongestPalindromicSubstring
+    {
+        /// <summary>
+        /// Given a string s, return the longest palindromic substring in s.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public string LongestPalindrome(string s)
+        {
+            if (s.Length <= 1)
+            {
+                return s;
+            }
+            string result = "";
+            for (int i = 0; i < s.Length; i++)
+            {
+                string temp = ExpandAroundCenter(s, i);
+                if (temp.Length > result.Length)
+                {
+                    result = temp;
+                }
+            }
+            return result;
+        }
+        private static string ExpandAroundCenter(string s, int cur)
+        {
+            int left = cur, right = cur;
+            while (left > 0 && s[cur] == s[left - 1])
+            {
+                left--;
+            }
+            while (right < s.Length - 1 && s[cur] == s[right + 1])
+            {
+                right++;
+            }
+            while (left > 0 && right < s.Length - 1)
+            {
+                if (s[--left] != s[++right])
+                {
+                    left++;
+                    right--;
+                    break;
+                }
+            }
+            return s.Substring(left, right - left + 1);
+        }
+    }
+}
---
+++ Project/Tests/Medium/LongestPalindromicSubstringTests.cs
@@ -0,0 +1,44 @@
+﻿using Algorithm.Medium;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tests.Medium
+{
+    public class LongestPalindromicSubstringTests
+    {
+        LongestPalindromicSubstring _member;
+        [SetUp]
+        public void SetUp()
+        {
+            _member = new LongestPalindromicSubstring();
+        }
+
+        [Test]
+        public void LongestPalindrome()
+        {
+            string input1 = "babad";
+            string input2 = "cbbd";
+            string input3 = "a";
+            string input4 = "ac";
+            string input5 = "";
+            string input6 = "aaaaaa";
+            string input7 = "cabacddae";
+            string expect1 = "bab"; //"bab" or "aba"
+            string expect2 = "bb";
+            string expect3 = "a";
+            string expect4 = "a";
+            string expect5 = "";
+            string expect6 = "aaaaaa";
+            string expect7 = "cabac";
+            Assert.AreEqual(expect1,_member.LongestPalindrome(input1));
+            Assert.AreEqual(expect2,_member.LongestPalindrome(input2));
+            Assert.AreEqual(expect3,_member.LongestPalindrome(input3));
+            Assert.AreEqual(expect4,_member.LongestPalindrome(input4));
+            Assert.AreEqual(expect5, _member.LongestPalindrome(input5));
+            Assert.AreEqual(expect6, _member.LongestPalindrome(input6));
+            Assert.AreEqual(expect7, _member.LongestPalindrome(input7));
+        }
+    }
+}
