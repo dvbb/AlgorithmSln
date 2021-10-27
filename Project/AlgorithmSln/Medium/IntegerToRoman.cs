89bb30202626752fe9d76f4c9ee40efb0b1a6e76
Chengming Ruan <v-cruan@microsoft.com>
2021-10-27 14:18:09 +0800

    Add 12.Integer To Roman(Medium)
    68 ms, 在所有 C# 提交中击败了100.00%的用户.赞

---
+++ Project/AlgorithmSln/Medium/IntegerToRoman.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.Medium
+{
+    public class IntegerToRoman
+    {
+        public string IntToRoman(int num)
+        {
+            string result = "";
+            int count = 10;
+            while (num != 0)
+            {
+                int temp = num % count;
+                if (temp > 0)
+                {
+                    if (count == 10)
+                    {
+                        result = ChangeRemainderToString(temp, "I", "V", "X") + result;
+                    }
+                    if (count == 100)
+                    {
+                        result = ChangeRemainderToString(temp / 10, "X", "L", "C") + result;
+                    }
+                    if (count == 1000)
+                    {
+                        result = ChangeRemainderToString(temp / 100, "C", "D", "M") + result;
+                    }
+                    if (count == 10000)
+                    {
+                        result = ChangeRemainderToString(temp / 1000, "M", "*", "*") + result;
+                    }
+                }
+                num -= temp;
+                count *= 10;
+            }
+            return result;
+        }
+
+        public static string ChangeRemainderToString(int num, string one, string five, string ten)
+        {
+            string result = "";
+            switch (num)
+            {
+                case 1:
+                    result = one;
+                    break;
+                case 2:
+                    result = one + one;
+                    break;
+                case 3:
+                    result = one + one + one;
+                    break;
+                case 4:
+                    result = one + five;
+                    break;
+                case 5:
+                    result = five;
+                    break;
+                case 6:
+                    result = five + one;
+                    break;
+                case 7:
+                    result = five + one + one;
+                    break;
+                case 8:
+                    result = five + one + one + one;
+                    break;
+                case 9:
+                    result = one + ten;
+                    break;
+            }
+            return result;
+        }
+    }
+}
---
+++ Project/Tests/Medium/IntegerToRomanTests.cs
@@ -0,0 +1,48 @@
+﻿using Algorithm.Medium;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlorithmTests.Medium
+{
+    public class IntegerToRomanTests
+    {
+        IntegerToRoman _member;
+        
+        [SetUp]
+        public void SetUp()
+        {
+            _member = new IntegerToRoman();
+        }
+
+        /// <summary>
+        /// 1 <= num <= 3999
+        /// </summary>
+        [Test]
+        public void IntToRoman()
+        {
+            int num1 = 3;
+            int num2 = 4;
+            int num3 = 9;
+            int num4 = 58;
+            int num5 = 1994;
+            int num6 = 1;
+            int num7 = 3999;
+            string expected1 = "III";
+            string expected2 = "IV";
+            string expected3 = "IX";
+            string expected4 = "LVIII";
+            string expected5 = "MCMXCIV";
+            string expected6 = "I";
+            string expected7 = "MMMCMXCIX";
+            Assert.AreEqual(expected1, _member.IntToRoman(num1));
+            Assert.AreEqual(expected2, _member.IntToRoman(num2));
+            Assert.AreEqual(expected3, _member.IntToRoman(num3));
+            Assert.AreEqual(expected4, _member.IntToRoman(num4));
+            Assert.AreEqual(expected5, _member.IntToRoman(num5));
+            Assert.AreEqual(expected6, _member.IntToRoman(num6));
+            Assert.AreEqual(expected7, _member.IntToRoman(num7));
+        }
+    }
+}
