60348870d92c2919497004f120e08ebd18b8c9c6
Chengming Ruan <v-cruan@microsoft.com>
2021-10-27 10:33:30 +0800

    Add 11.Container With Most Water(Medium)

---
+++ Project/AlgorithmSln/Medium/ContainerWithMostWater.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.Medium
+{
+    public class ContainerWithMostWater
+    {
+        /// <summary>
+        /// Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate(i, ai). 
+        /// n vertical lines are drawn such that the two endpoints of the line i is at(i, ai) and(i, 0). 
+        /// Find two lines, which, together with the x-axis forms a container, such that the container contains the most water.
+        /// </summary>
+        /// <param name="height"></param>
+        /// <returns></returns>
+        public int MaxArea(int[] height)
+        {
+            int i = 0;
+            int j = height.Length - 1;
+            int result = height[i] > height[j] ? (j - i) * height[j] : (j - i) * height[i];
+            while (i != j)
+            {
+                if (height[i] > height[j])
+                {
+                    j--;
+                }
+                else
+                {
+                    i++;
+                }
+                int temp = height[i] > height[j] ? (j - i) * height[j] : (j - i) * height[i];
+                result = temp > result ? temp : result;
+            }
+            return result;
+        }
+    }
+}
---
+++ Project/Tests/Medium/ContainerWithMostWaterTests.cs
@@ -0,0 +1,41 @@
+﻿using Algorithm.Medium;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlorithmTests.Medium
+{
+    public class ContainerWithMostWaterTests
+    {
+        ContainerWithMostWater _member;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _member = new ContainerWithMostWater();
+        }
+
+        /// <summary>
+        /// n == height.length
+        /// 2 <= n <= 105
+        /// 0 <= height[i] <= 104
+        /// </summary>
+        [Test]
+        public void MaxArea()
+        {
+            int[] height1 = new int[] { 1,8,6,2,5,4,8,3,7  };
+            int[] height2 = new int[] { 1, 1 };
+            int[] height3 = new int[] { 4, 3, 2, 1, 4 };
+            int[] height4 = new int[] { 1, 2, 1 };
+            int[] height5 = new int[] { 0, 0 };
+            int[] height6 = new int[] { 10, 0 ,10};
+            Assert.AreEqual(49,_member.MaxArea(height1));
+            Assert.AreEqual(1,_member.MaxArea(height2));
+            Assert.AreEqual(16,_member.MaxArea(height3));
+            Assert.AreEqual(2,_member.MaxArea(height4));
+            Assert.AreEqual(0,_member.MaxArea(height5));
+            Assert.AreEqual(20,_member.MaxArea(height6));
+        }
+    }
+}
