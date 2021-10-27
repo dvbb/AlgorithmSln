28fb8a92bbe943e8a3c59ef44e4573e60e9a6997
Chengming Ruan <v-cruan@microsoft.com>
2021-10-27 15:06:10 +0800

    Add 13. Roman to Integer (Easy)

---
+++ Project/AlgorithmSln/Easy/RomantoInteger.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.Easy
+{
+    public class RomantoInteger
+    {
+        /// <summary>
+        /// Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public int RomanToInt(string s)
+        {
+            int result = 0;
+            #region V1: Replace special number(Use HashTable)
+            //Hashtable table = new Hashtable()
+            //{
+            //    {"IV",4 },
+            //    {"IX",9 },
+            //    {"XL",40 },
+            //    {"XC",90 },
+            //    {"CD",400 },
+            //    {"CM",900 },
+            //};
+            //foreach (string item in table.Keys)
+            //{
+            //    if (s.Contains(item))
+            //    {
+            //        s = s.Replace(item, "");
+            //        result += (int)table[item];
+            //    }
+            //}
+            #endregion
+
+            // V2: Replace special number(Use Equivalent Exchange)
+            s = s.Replace("IV", "a");
+            s = s.Replace("IX", "b");
+            s = s.Replace("XL", "c");
+            s = s.Replace("XC", "d");
+            s = s.Replace("CD", "e");
+            s = s.Replace("CM", "f");
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                result += GetCharValue(s[i]);
+            }
+            return result;
+        }
+
+        public static int GetCharValue(char ch)
+        {
+            switch (ch)
+            {
+                case 'I': return 1;
+                case 'V': return 5;
+                case 'X': return 10;
+                case 'L': return 50;
+                case 'C': return 100;
+                case 'D': return 500;
+                case 'M': return 1000;
+                case 'a': return 4;
+                case 'b': return 9;
+                case 'c': return 40;
+                case 'd': return 90;
+                case 'e': return 400;
+                case 'f': return 900;
+                default: return 0;
+            }
+        }
+    }
+}
---
+++ Project/Tests/Easy/RomantoIntegerTests.cs
@@ -0,0 +1,43 @@
+﻿using Algorithm.Easy;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlorithmTests.Easy
+{
+    public class RomantoIntegerTests
+    {
+        private RomantoInteger _member;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _member = new RomantoInteger();
+        }
+
+        /// <summary>
+        /// 1 <= s.length <= 15
+        //  s contains only the characters('I', 'V', 'X', 'L', 'C', 'D', 'M').
+        //  It is guaranteed that s is a valid roman numeral in the range[1, 3999].
+        /// </summary>
+        [Test]
+        public void RomanToInt()
+        {
+            string s1 = "III";
+            string s2 = "IV";
+            string s3 = "IX";
+            string s4 = "LVIII";
+            string s5 = "MCMXCIV";
+            string s6 = "I";
+            string s7 = "MMMCMXCIX";
+            Assert.AreEqual(3, _member.RomanToInt(s1));
+            Assert.AreEqual(4, _member.RomanToInt(s2));
+            Assert.AreEqual(9, _member.RomanToInt(s3));
+            Assert.AreEqual(58, _member.RomanToInt(s4));
+            Assert.AreEqual(1994, _member.RomanToInt(s5));
+            Assert.AreEqual(1, _member.RomanToInt(s6));
+            Assert.AreEqual(3999, _member.RomanToInt(s7));
+        }
+    }
+}
