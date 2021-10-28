6f2bc6c14ee3a79e3bf220784036a09122fbb393
Chengming Ruan <v-cruan@microsoft.com>
2021-10-28 11:38:43 +0800

    Add 14. Longest Common Prefix (Easy)

---
+++ Project/AlgorithmSln/Easy/LongestCommonPrefix.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.Easy
+{
+    public class LongestCommonPrefix
+    {
+        /// <summary>
+        /// Write a function to find the longest common prefix string amongst an array of strings.
+        /// If there is no common prefix, return an empty string "".
+        /// </summary>
+        /// <param name="strs"></param>
+        /// <returns></returns>
+        public string GetLongestCommonPrefix(string[] strs)
+        {
+            string prefix = strs[0];
+            if (strs.Length == 1)
+            {
+                return prefix;
+            }
+            for (int i = 1; i < strs.Length; i++)
+            {
+                prefix = prefix.Length > strs[i].Length ? strs[i] : prefix;
+            }
+            for (int i = 0; i < strs.Length; i++)
+            {
+                int j = 0;
+                while (j < strs[i].Length && j < prefix.Length)
+                {
+
+                    prefix = prefix[j] == strs[i][j] ? prefix : prefix.Substring(0, j);
+                    j++;
+                }
+            }
+            return prefix;
+        }
+    }
+}
---
+++ Project/Tests/Easy/LongestCommonPrefixTests.cs
@@ -0,0 +1,47 @@
+﻿using Algorithm.Easy;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlorithmTests.Easy
+{
+    public class LongestCommonPrefixTests
+    {
+        private LongestCommonPrefix _member;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _member = new LongestCommonPrefix();
+        }
+
+        /// <summary>
+        /// 1 <= strs.length <= 200
+        /// 0 <= strs[i].length <= 200
+        /// strs[i] consists of only lower-case English letters.
+        /// </summary>
+        [Test]
+        public void GetLongestCommonPrefix()
+        {
+            string[] strs1 = new string[] { "flower", "flow", "flight" };
+            string[] strs2 = new string[] { "dog", "racecar", "car" };
+            string[] strs3 = new string[] { "dog"};
+            string[] strs4 = new string[] { ""};
+            string[] strs5 = new string[] { "ab","a"};
+            string[] strs6 = new string[] { "reflower", "flow", "flight" };
+            string expected1 = "fl";
+            string expected2 = "";
+            string expected3 = "dog";
+            string expected4 = "";
+            string expected5 = "a";
+            string expected6 = "";
+            Assert.AreEqual(expected1, _member.GetLongestCommonPrefix(strs1));
+            Assert.AreEqual(expected2, _member.GetLongestCommonPrefix(strs2));
+            Assert.AreEqual(expected3, _member.GetLongestCommonPrefix(strs3));
+            Assert.AreEqual(expected4, _member.GetLongestCommonPrefix(strs4));
+            Assert.AreEqual(expected5, _member.GetLongestCommonPrefix(strs5));
+            Assert.AreEqual(expected6, _member.GetLongestCommonPrefix(strs6));
+        }
+    }
+}
