5bc1faf28323c235b50243170d8dd8b514cfe2b7
Chengming Ruan <v-cruan@microsoft.com>
2021-10-29 16:58:42 +0800

    Add 15.ThreeSum (Medium)

---
+++ Project/AlgorithmSln/Medium/ThreeSum.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Algorithm.Medium
+{
+    public class ThreeSum
+    {
+        #region GetThreeSumV1
+        /// <summary>
+        /// Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+        /// Notice that the solution set must not contain duplicate triplets.
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public IList<IList<int>> GetThreeSum(int[] nums)
+        {
+            List<IList<int>> result = new List<IList<int>>() { };
+            Hashtable ht = new Hashtable();
+            nums = BubbleSort(nums);
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (!ht.ContainsKey($"{nums[i]},{nums[j]}"))
+                    {
+                        ht.Add($"{nums[i]},{nums[j]}", nums[i] + nums[j]);
+                    }
+                }
+            }
+            Hashtable isUnique = new Hashtable();
+            foreach (var item in ht.Keys)
+            {
+                string[] strs = ((string)item).Split(',');
+                var ergodicNum = MyExcept(nums, strs);
+                foreach (var num in ergodicNum)
+                {
+                    if (num == (int)ht[item] * -1)
+                    {
+                        int[] keynums = new int[] { int.Parse(strs[0]), int.Parse(strs[1]) };
+                        if (keynums[0] + keynums[1] >= 0)
+                        {
+                            int[] curnums1 = new int[] { num, keynums[0], keynums[1] };
+                            if (!isUnique.ContainsKey($"{num},{keynums[0]},{keynums[1]}") && num <= keynums[0] && keynums[0] <= keynums[1])
+                            {
+                                isUnique.Add($"{num},{keynums[0]},{keynums[1]}", "");
+                                List<int> line1 = new List<int>(curnums1);
+                                result.Add(line1);
+                            }
+                        }
+                        else
+                        {
+                            int[] curnums2 = new int[] { keynums[0], keynums[1], num };
+                            if (!isUnique.ContainsKey($"{keynums[0]},{keynums[1]},{num}") && keynums[0] <= keynums[1] && keynums[1] <= num)
+                            {
+                                isUnique.Add($"{keynums[0]},{keynums[1]},{num}", "");
+                                List<int> line2 = new List<int>(curnums2);
+                                result.Add(line2);
+                            }
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+        public int[] BubbleSort(int[] nums)
+        {
+            int temp;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (nums[j] > nums[i])
+                    {
+                        temp = nums[i];
+                        nums[i] = nums[j];
+                        nums[j] = temp;
+                    }
+                }
+            }
+            return nums;
+        }
+        public static List<int> MyExcept(int[] nums, string[] strs)
+        {
+            bool flag0 = true;
+            bool flag1 = true;
+            List<int> result = new List<int>();
+            foreach (var item in nums)
+            {
+                if (item.ToString() == strs[0] && flag0)
+                {
+                    flag0 = false;
+                    continue;
+                }
+                if (item.ToString() == strs[1] && flag1)
+                {
+                    flag1 = false;
+                    continue;
+                }
+                result.Add(item);
+            }
+            return result;
+        }
+        #endregion
+
+        #region GetThreeSumV2
+        /// <summary>
+        /// ref:https://leetcode-cn.com/problems/3sum/solution/pai-xu-shuang-zhi-zhen-fa-by-zclmoon-o7ur/
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public IList<IList<int>> GetThreeSumV2(int[] nums)
+        {
+            var result = new List<IList<int>>();
+            if (nums == null || nums.Length < 3) return result;
+            Array.Sort(nums); //Time Complexity: O(nlogn)
+            for (int cur = 0; cur < nums.Length - 2; cur++)
+            {
+                if (nums[cur] > 0)
+                {
+                    break;
+                }
+                if (cur > 0 && nums[cur] == nums[cur - 1]) continue;
+                int i = cur + 1;
+                int j = nums.Length - 1;
+                while (i < j)
+                {
+                    int sum = nums[cur] + nums[i] + nums[j];
+
+                    if (sum < 0)
+                    {
+                        while (i < j && nums[i] == nums[++i]);
+                    }
+                    else if (sum > 0)
+                    {
+                        while (i < j && nums[j] == nums[--j]);
+                    }
+                    else
+                    {
+                        result.Add(new List<int>() { nums[cur], nums[i], nums[j] });
+                        while (i < j && nums[i] == nums[++i]) ; 
+                        while (i < j && nums[j] == nums[--j]) ; 
+                    }
+                }
+            }
+            return result;
+        }
+        #endregion
+    }
+}
---
+++ Project/Tests/Medium/ThreeSumTests.cs
@@ -0,0 +1,81 @@
+﻿using Algorithm.Medium;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlorithmTests.Medium
+{
+    public class ThreeSumTests
+    {
+        private ThreeSum _member;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _member = new ThreeSum();
+        }
+
+        /// <summary>
+        /// 0 <= nums.length <= 3000
+        /// -105 <= nums[i] <= 105
+        /// </summary>
+        [Test]
+        public void GetThreeSum()
+        {
+            int[] num1 = new int[] { -1, 0, 1, 2, -1, -4 };
+            int[] num2 = new int[] { };
+            int[] num3 = new int[] { 0 };
+            int[] num4 = new int[] { 105, -105, 0 };
+            int[] num5 = new int[] { -1, 2, 0 };
+            int[] num6 = new int[] { 9, -8, 7, -1, -1, -1, 0, 6 };
+            int[] num7 = new int[] { 0, 0, 0 };
+            int[] num8 = new int[] { 1, 0, -1 };
+            List<IList<int>> expected1 = new List<IList<int>>() { new List<int>() { -1, 0, 1 },new List<int>() { -1, -1, 2 }  };
+            List<IList<int>> expected2 = new List<IList<int>>() { };
+            List<IList<int>> expected3 = new List<IList<int>>() { };
+            List<IList<int>> expected4 = new List<IList<int>>() { new List<int>() { -105, 0, 105 } };
+            List<IList<int>> expected5 = new List<IList<int>>() { };
+            List<IList<int>> expected6 = new List<IList<int>>() { new List<int>() { -8, -1, 9 } };
+            List<IList<int>> expected7 = new List<IList<int>>() { new List<int>() { 0, 0, 0 } };
+            List<IList<int>> expected8 = new List<IList<int>>() { new List<int>() { -1, 0, 1 } };
+            Assert.AreEqual(expected1, _member.GetThreeSum(num1));
+            Assert.AreEqual(expected2, _member.GetThreeSum(num2));
+            Assert.AreEqual(expected3, _member.GetThreeSum(num3));
+            Assert.AreEqual(expected4, _member.GetThreeSum(num4));
+            Assert.AreEqual(expected5, _member.GetThreeSum(num5));
+            Assert.AreEqual(expected6, _member.GetThreeSum(num6));
+            Assert.AreEqual(expected7, _member.GetThreeSum(num7));
+            Assert.AreEqual(expected8, _member.GetThreeSum(num8));
+        }
+
+        [Test]
+        public void GetThreeSumV2()
+        {
+            int[] num1 = new int[] { -1, 0, 1, 2, -1, -4 };
+            int[] num2 = new int[] { };
+            int[] num3 = new int[] { 0 };
+            int[] num4 = new int[] { 105, -105, 0 };
+            int[] num5 = new int[] { -1, 2, 0 };
+            int[] num6 = new int[] { 9, -8, 7, -1, -1, -1, 0, 6 };
+            int[] num7 = new int[] { 0, 0, 0 };
+            int[] num8 = new int[] { 1, 0, -1 };
+            List<IList<int>> expected1 = new List<IList<int>>() { new List<int>() { -1, -1, 2 }, new List<int>() { -1, 0, 1 } };
+            List<IList<int>> expected2 = new List<IList<int>>() { };
+            List<IList<int>> expected3 = new List<IList<int>>() { };
+            List<IList<int>> expected4 = new List<IList<int>>() { new List<int>() { -105, 0, 105 } };
+            List<IList<int>> expected5 = new List<IList<int>>() { };
+            List<IList<int>> expected6 = new List<IList<int>>() { new List<int>() { -8, -1, 9 } };
+            List<IList<int>> expected7 = new List<IList<int>>() { new List<int>() { 0, 0, 0 } };
+            List<IList<int>> expected8 = new List<IList<int>>() { new List<int>() { -1, 0, 1 } };
+            Assert.AreEqual(expected1, _member.GetThreeSumV2(num1));
+            Assert.AreEqual(expected2, _member.GetThreeSumV2(num2));
+            Assert.AreEqual(expected3, _member.GetThreeSumV2(num3));
+            Assert.AreEqual(expected4, _member.GetThreeSumV2(num4));
+            Assert.AreEqual(expected5, _member.GetThreeSumV2(num5));
+            Assert.AreEqual(expected6, _member.GetThreeSumV2(num6));
+            Assert.AreEqual(expected7, _member.GetThreeSumV2(num7));
+            Assert.AreEqual(expected8, _member.GetThreeSumV2(num8));
+        }
+    }
+}
