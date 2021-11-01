b7cb292e1a041d9268b5c8c5dd497bc1be6579e0
Chengming Ruan <v-cruan@microsoft.com>
2021-11-01 16:42:49 +0800

    Add 16.ThreeSumClosest (Medium)
    80 ms. 97.46%

---
+++ Project/AlgorithmSln/Medium/ThreeSumClosest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.Medium
+{
+    public class ThreeSumClosest
+    {
+        /// <summary>
+        /// Given an integer array nums of length n and an integer target, find three integers in nums such that the sum is closest to target.
+        /// Return the sum of the three integers.
+        /// You may assume that each input would have exactly one solution.
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public int GetThreeSumClosest(int[] nums, int target)
+        {
+            int result = nums[0] + nums[1] + nums[2];
+            int diff;
+            Array.Sort(nums);
+            for (int i = 1; i < nums.Length-1; i++)
+            {
+                int left = i - 1;
+                int right = i + 1;
+                while (left >= 0 && right < nums.Length)
+                {
+                    diff = target - (nums[i] + nums[left] + nums[right]);
+                    result = Math.Abs( target - result) < Math.Abs( diff) ? result : nums[i] + nums[left] + nums[right];
+                    if (diff == 0) break;
+                    if (diff > 0) right++;
+                    if (diff < 0) left--;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ Project/Tests/Medium/ThreeSumClosestTests.cs
@@ -0,0 +1,43 @@
+﻿using Algorithm.Medium;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlorithmTests.Medium
+{
+    public class ThreeSumClosestTests
+    {
+        private ThreeSumClosest _member;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _member = new ThreeSumClosest();
+        }
+
+        /// <summary>
+        /// 3 <= nums.length <= 1000
+        /// -1000 <= nums[i] <= 1000
+        /// -104 <= target <= 104
+        /// </summary>
+        [Test]
+        public void GetThreeSumClosest()
+        {
+            int[] nums1 = new int[] { -1, 2, 1, -4 };
+            int[] nums2 = new int[] { 0, 0, 0 };
+            int[] nums3 = new int[] { -1000, 1000, -1, 2, 1, 1 };
+            int[] nums4 = new int[] { 99, 10, -7, 0, 6 };
+            int[] nums5 = new int[] { 99, 10, -7, 0, 6 };
+            int[] nums6 = new int[] { 104, 0, 1, 0, 1,30 ,-12,-99,999};
+            int[] nums7 = new int[] { 1, 2, 5, 10, 11 };
+            Assert.AreEqual(2, _member.GetThreeSumClosest(nums1, 1));
+            Assert.AreEqual(0, _member.GetThreeSumClosest(nums2, 1));
+            Assert.AreEqual(1, _member.GetThreeSumClosest(nums3, 1));
+            Assert.AreEqual(-1, _member.GetThreeSumClosest(nums4, -104));
+            Assert.AreEqual(105, _member.GetThreeSumClosest(nums5, 104));
+            Assert.AreEqual(35, _member.GetThreeSumClosest(nums6, 56));
+            Assert.AreEqual(13, _member.GetThreeSumClosest(nums7, 12));
+        }
+    }
+}
