52651effe78767b7dc5bfdb238d821bf3fda91d5
Chengming Ruan <v-cruan@microsoft.com>
2021-11-02 10:24:23 +0800

    Add 17. Letter Combinations of a Phone Number (Medium)
    136 ms. 89.67%

---
+++ Project/AlgorithmSln/Medium/LetterCombinations.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.Medium
+{
+    public class LetterCombinations
+    {
+        /// <summary>
+        /// Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
+        /// Mapping:
+        ///     2:abc 3:def 4:ghi 5:jkl 6:mno 7:pqrs 8:tuv 9:wxyz
+        /// </summary>
+        /// <param name="digits"></param>
+        /// <returns></returns>
+        public IList<string> GetLetterCombinations(string digits)
+        {
+            if (digits.Length == 0)
+            {
+                return new List<string>();
+            }
+            return GetATree(new List<string>(), digits, 0);
+        }
+
+        public List<String> GetATree(List<String> list, string digits, int i)
+        {
+            if (i == digits.Length)
+            {
+                return list;
+            }
+            string str = GetMapping(digits[i]);
+            if (list.Count == 0)
+            {
+                for (int cur = 0; cur < str.Length; cur++)
+                {
+                    list.Add(str[cur].ToString());
+                }
+                return GetATree(list, digits, i + 1);
+            }
+            else
+            {
+                List<string> addonsList = new List<string>();
+                foreach (var item in list)
+                {
+                    for (int cur = 0; cur < str.Length; cur++)
+                    {
+                        addonsList.Add(item + str[cur].ToString());
+                    }
+                }
+                return GetATree(addonsList, digits, i + 1);
+            }
+        }
+
+        public string GetMapping(char num)
+        {
+            switch (num)
+            {
+                case '2': return "abc";
+                case '3': return "def";
+                case '4': return "ghi";
+                case '5': return "jkl";
+                case '6': return "mno";
+                case '7': return "pqrs";
+                case '8': return "tuv";
+                case '9': return "wxyz";
+                default: return "";
+            }
+        }
+    }
+}
---
+++ Project/Tests/Medium/LetterCombinationsTests.cs
@@ -0,0 +1,49 @@
+﻿using Algorithm.Medium;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlorithmTests.Medium
+{
+    public class LetterCombinationsTests
+    {
+        private LetterCombinations _member;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _member = new LetterCombinations();
+        }
+
+
+
+
+        /// <summary>
+        /// 0 <= digits.length <= 4
+        /// digits[i] is a digit in the range['2', '9'].
+        /// </summary>
+        [Test]
+        public void GetLetterCombinations()
+        {
+            string digits1 = "23";
+            string digits2 = "";
+            string digits3 = "2";
+            string digits4 = "2289";
+            string digits5 = "9";
+            string digits6 = "44";
+            List<string> expected1 = new List<string>() { "ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf" };
+            List<string> expected2 = new List<string>() { };
+            List<string> expected3 = new List<string>() { "a", "b", "c" };
+            List<string> expected4 = new List<string>() {"aatw","aatx","aaty","aatz","aauw","aaux","aauy","aauz","aavw","aavx","aavy","aavz","abtw","abtx","abty","abtz","abuw","abux","abuy","abuz","abvw","abvx","abvy","abvz","actw","actx","acty","actz","acuw","acux","acuy","acuz","acvw","acvx","acvy","acvz","batw","batx","baty","batz","bauw","baux","bauy","bauz","bavw","bavx","bavy","bavz","bbtw","bbtx","bbty","bbtz","bbuw","bbux","bbuy","bbuz","bbvw","bbvx","bbvy","bbvz","bctw","bctx","bcty","bctz","bcuw","bcux","bcuy","bcuz","bcvw","bcvx","bcvy","bcvz","catw","catx","caty","catz","cauw","caux","cauy","cauz","cavw","cavx","cavy","cavz","cbtw","cbtx","cbty","cbtz","cbuw","cbux","cbuy","cbuz","cbvw","cbvx","cbvy","cbvz","cctw","cctx","ccty","cctz","ccuw","ccux","ccuy","ccuz","ccvw","ccvx","ccvy","ccvz" };
+            List<string> expected5 = new List<string>() { "w", "x", "y", "z" };
+            List<string> expected6 = new List<string>() { "gg", "gh", "gi", "hg", "hh", "hi", "ig", "ih", "ii" };
+            Assert.AreEqual(expected1, _member.GetLetterCombinations(digits1));
+            Assert.AreEqual(expected2, _member.GetLetterCombinations(digits2));
+            Assert.AreEqual(expected3, _member.GetLetterCombinations(digits3));
+            Assert.AreEqual(expected4, _member.GetLetterCombinations(digits4));Assert.AreEqual(108, _member.GetLetterCombinations(digits4).Count);
+            Assert.AreEqual(expected5, _member.GetLetterCombinations(digits5));
+            Assert.AreEqual(expected6, _member.GetLetterCombinations(digits6));
+        }
+    }
+}
