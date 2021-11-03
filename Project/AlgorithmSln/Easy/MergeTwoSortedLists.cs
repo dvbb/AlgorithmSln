4e8ac4c145cb0b65220b3104979e195336437d9b
Chengming Ruan <v-cruan@microsoft.com>
2021-11-03 17:23:55 +0800

    Update 21.V2
    Recursive. 92ms 34.78%

--- Project/AlgorithmSln/Easy/MergeTwoSortedLists.cs
+++ Project/AlgorithmSln/Easy/MergeTwoSortedLists.cs
@@ -56,5 +56,28 @@ namespace Algorithm.Easy
             cur.next = null;
             return result;
         }
+
+        /// <summary>
+        /// Recursive
+        /// </summary>
+        /// <param name="l1"></param>
+        /// <param name="l2"></param>
+        /// <returns></returns>
+        public ListNode MergeTwoListsV2(ListNode l1, ListNode l2)
+        {
+            if (l1 == null) return l2;
+            if (l2 == null) return l1;
+            if (l1.val < l2.val)
+            {
+                l1.next = MergeTwoListsV2(l1.next, l2);
+                return l1;
+            }
+            else
+            {
+                l2.next = MergeTwoListsV2(l1, l2.next);
+                return l2;
+            }
+        }
+
     }
 }
--- Project/Tests/Easy/MergeTwoSortedListTests.cs
+++ Project/Tests/Easy/MergeTwoSortedListTests.cs
@@ -50,5 +50,34 @@ namespace AlorithmTests.Easy
             Assert.AreEqual(MyFormat.NodeToString(expectedList3), MyFormat.NodeToString(_member.MergeTwoLists(inputL31, inputL32)));
             Assert.AreEqual(MyFormat.NodeToString(expectedList4), MyFormat.NodeToString(_member.MergeTwoLists(inputL41, inputL42)));
         }
+
+        [Test]
+        public void MergeTwoListsV2()
+        {
+            //l1 = [1, 2, 4], l2 = [1, 3, 4]
+            ListNode inputL11 = ListNode.CreateByIntArray(new int[] { 1, 2, 4 });
+            ListNode inputL12 = ListNode.CreateByIntArray(new int[] { 1, 3, 4 });
+            ListNode expectedList1 = ListNode.CreateByIntArray(new int[] { 1, 1, 2, 3, 4, 4 });
+
+            //l1 = [], l2 = []
+            ListNode inputL21 = ListNode.CreateByIntArray(null);
+            ListNode inputL22 = ListNode.CreateByIntArray(null);
+            ListNode expectedList2 = ListNode.CreateByIntArray(null);
+
+            //l1 = [], l2 = [0]
+            ListNode inputL31 = ListNode.CreateByIntArray(null);
+            ListNode inputL32 = ListNode.CreateByIntArray(new int[] { 0 });
+            ListNode expectedList3 = ListNode.CreateByIntArray(new int[] { 0 });
+
+            //l1 = [-100,-2,0,1,1,25,100], l2 = [-75,-22,1]
+            ListNode inputL41 = ListNode.CreateByIntArray(new int[] { -100, -2, 0, 1, 1, 25, 100 });
+            ListNode inputL42 = ListNode.CreateByIntArray(new int[] { -75, -22, 1 });
+            ListNode expectedList4 = ListNode.CreateByIntArray(new int[] { -100, -75, -22, -2, 0, 1, 1, 1, 25, 100 });
+
+            Assert.AreEqual(MyFormat.NodeToString(expectedList1), MyFormat.NodeToString(_member.MergeTwoListsV2(inputL11, inputL12)));
+            Assert.AreEqual(MyFormat.NodeToString(expectedList2), MyFormat.NodeToString(_member.MergeTwoListsV2(inputL21, inputL22)));
+            Assert.AreEqual(MyFormat.NodeToString(expectedList3), MyFormat.NodeToString(_member.MergeTwoListsV2(inputL31, inputL32)));
+            Assert.AreEqual(MyFormat.NodeToString(expectedList4), MyFormat.NodeToString(_member.MergeTwoListsV2(inputL41, inputL42)));
+        }
     }
 }
