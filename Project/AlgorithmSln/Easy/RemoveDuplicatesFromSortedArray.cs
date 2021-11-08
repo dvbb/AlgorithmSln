5848d59c396905cc9c94edc73d7357d54db7ef26
Chengming Ruan <v-cruan@microsoft.com>
2021-11-08 16:31:56 +0800

    Add 26. Remove Duplicates from Sorted Array (Easy)
    136ms 87.47%

---
+++ Project/AlgorithmSln/Easy/RemoveDuplicatesFromSortedArray.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.Easy
+{
+    public class RemoveDuplicatesFromSortedArray
+    {
+        /// <summary>
+        /// 26. Remove Duplicates from Sorted Array
+        /// Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once.
+        /// The relative order of the elements should be kept the same.
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int RemoveDuplicates(int[] nums)
+        {
+            if (nums.Length == 0) return 0;
+            if (nums.Length == 1) return 1;
+            int total = 1;
+            int temp = 0;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i]>nums[temp])
+                {
+                    nums[temp+1] = nums[i];
+                    temp ++;
+                    total++;
+                }
+            }
+            return total;
+        }
+    }
+}
---
+++ Project/Tests/Easy/RemoveDuplicatesFromSortedArrayTests.cs
@@ -0,0 +1,49 @@
+﻿using Algorithm.Easy;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlorithmTests.Easy
+{
+    public class RemoveDuplicatesFromSortedArrayTests
+    {
+        private RemoveDuplicatesFromSortedArray _member;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _member = new RemoveDuplicatesFromSortedArray();
+        }
+
+        /// <summary>
+        /// 0 <= nums.length <= 3 * 104
+        /// -100 <= nums[i] <= 100
+        /// nums is sorted in non-decreasing order.
+        /// </summary>
+        [Test]
+        public void RemoveDuplicates()
+        {
+            int[] nums1 = new int[] { 1, 1, 2 };
+            int expected1 = 2;
+
+            int[] nums2 = new int[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+            int expected2 = 5;
+
+            int[] nums3 = new int[] { };
+            int expected3 = 0;
+
+            int[] nums4 = new int[] { 1 };
+            int expected4 = 1;
+
+            int[] nums5 = new int[] { -100, -56, -56, -21, -12,0,0, 0, 2, 2, 8, 15, 15, 26, 26, 26, 52, 99, 99, 99, 100 };
+            int expected5 = 12;
+
+            Assert.AreEqual(expected1, _member.RemoveDuplicates(nums1));
+            Assert.AreEqual(expected2, _member.RemoveDuplicates(nums2));
+            Assert.AreEqual(expected3, _member.RemoveDuplicates(nums3));
+            Assert.AreEqual(expected4, _member.RemoveDuplicates(nums4));
+            Assert.AreEqual(expected5, _member.RemoveDuplicates(nums5));
+        }
+    }
+}
