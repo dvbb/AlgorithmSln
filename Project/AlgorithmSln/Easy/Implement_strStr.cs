41cafe825b983edd85cfc1fcd651c2da5ded453c
Chengming Ruan <v-cruan@microsoft.com>
2021-11-10 17:46:13 +0800

    Add 28. Implement strStr (Easy)
    448 ms 61%

---
+++ Project/AlgorithmSln/Easy/Implement_strStr.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.Easy
+{
+    public class Implement_strStr
+    {
+        /// <summary>
+        /// Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+        /// </summary>
+        /// <param name="haystack"></param>
+        /// <param name="needle"></param>
+        /// <returns></returns>
+        public int StrStr(string haystack, string needle)
+        {
+            if (needle == "") return 0;
+            if (needle != "" && haystack == "") return -1;
+            if (!haystack.Contains(needle)) return -1;
+            return haystack.IndexOf(needle);
+        }
+
+        public int StrStrV2(string haystack, string needle)
+        {
+            if (needle == "") return 0;
+            if (needle != "" && haystack == "") return -1;
+            if (!haystack.Contains(needle)) return -1;
+            return haystack.IndexOf(needle);
+        }
+    }
+}
---
+++ Project/Tests/Easy/Implement_strStrTests.cs
@@ -0,0 +1,42 @@
+﻿using Algorithm.Easy;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlorithmTests.Easy
+{
+    public class Implement_strStrTests
+    {
+        private Implement_strStr _member;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _member = new Implement_strStr();
+        }
+
+        /// <summary>
+        /// 0 <= haystack.length, needle.length <= 5 * 104
+        /// haystack and needle consist of only lower-case English characters.
+        /// </summary>
+        [Test]
+        public void strStr()
+        {
+            string haystack1 = "hello", needle1 = "ll";
+            string haystack2 = "aaaaa", needle2 = "bba";
+            string haystack3 = "",needle3 = "";
+            string haystack4 = "",needle4 = "adx";
+            string haystack5 = "dfs",needle5 = "";
+            string haystack6 = "abccdsccx",needle6 = "ccx";
+            string haystack7 = "avvbvvcsxcxdfvvbvvxs",needle7 = "avv";
+            Assert.AreEqual(2,_member.StrStr(haystack1,needle1));
+            Assert.AreEqual(-1,_member.StrStr(haystack2,needle2));
+            Assert.AreEqual(0,_member.StrStr(haystack3,needle3));
+            Assert.AreEqual(-1,_member.StrStr(haystack4,needle4));
+            Assert.AreEqual(0,_member.StrStr(haystack5,needle5));
+            Assert.AreEqual(6,_member.StrStr(haystack6,needle6));
+            Assert.AreEqual(0,_member.StrStr(haystack7,needle7));
+        }
+    }
+}
