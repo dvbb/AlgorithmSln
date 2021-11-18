11393eedf5902f3fa743afcec3fb9afe70821b17
Chengming Ruan <v-cruan@microsoft.com>
2021-11-18 14:05:15 +0800

    Add 46.Permutations (Medium)
    140ms 67%

---
+++ Project/AlgorithmSln/Medium/Permutations.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.Medium
+{
+    public class Permutations
+    {
+        /// <summary>
+        /// Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public IList<IList<int>> Permute(int[] nums)
+        {
+            if (nums.Length == 0 || nums == null)
+                return new List<IList<int>>() { };
+            IList<IList<int>> result = new List<IList<int>>() { };
+            LinkedList<int> line = new LinkedList<int>();
+            BackTrack(result, nums, line);
+            return result;
+        }
+        private static void BackTrack(IList<IList<int>> result, int[] nums, LinkedList<int> line)
+        {
+            if (line.Count == nums.Length)
+            {
+                result.Add(new List<int>(line));
+                return;
+            }
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (line.Contains(nums[i]))
+                {
+                    continue;
+                }
+                line.AddLast(nums[i]);
+                BackTrack(result, nums, line);
+                line.Remove(nums[i]);
+            }
+        }
+    }
+}
---
+++ Project/Tests/Medium/PermutationsTests.cs
@@ -0,0 +1,40 @@
+﻿using Algorithm.Medium;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlorithmTests.Medium
+{
+    public class PermutationsTests
+    {
+        private Permutations _member;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _member = new Permutations();
+        }
+
+        /// <summary>
+        /// 1 <= nums.length <= 6
+        /// -10 <= nums[i] <= 10
+        /// All the integers of nums are unique.
+        /// </summary>
+        [Test]
+        public void PermuteTests()
+        {
+            int[] nums1 = new int[] { 1, 2, 3 };
+            int[] nums2 = new int[] { 0, 1 };
+            int[] nums3 = new int[] { 1 };
+            IList<IList<int>> list1 = _member.Permute(nums1);
+            foreach (var item in list1)
+            {
+                Console.WriteLine(item);
+            }
+            //[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+            //[[0,1],[1,0]]
+            //[[1]]
+        }
+    }
+}
