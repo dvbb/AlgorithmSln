24194abd64ea3557169b5ab6996d43f552092497
Chengming Ruan (WICRESOFT NORTH AMERICA LTD) <v-cruan@microsoft.com>
2022-03-03 16:44:51 +0800

    Add 53. Maximum Subarray (Easy)
    188 ms 73.91%

---
+++ Project/AlgorithmSln/Easy/MaximumSubarray.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Algorithm.Easy
+{
+    public class MaximumSubarray
+    {
+        /// <summary>
+        /// Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+        /// A subarray is a contiguous part of an array.
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int MaxSubArray(int[] nums)
+        {
+            if (nums.Length == 0)
+                throw new ArgumentNullException("Input Array Cannot Be Null Or Empty");
+            if (nums.Length == 1)
+                return nums[0];
+
+            int max = nums[0];
+            int sum = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] > sum + nums[i])
+                {
+                    sum = nums[i];
+                }
+                else 
+                {
+                    sum += nums[i];
+                }
+                max = max > sum ? max : sum;
+            }
+            return max;
+        }
+    }
+}
---
+++ Project/Tests/Easy/MaximumSubarrayTests.cs
@@ -0,0 +1,49 @@
+﻿using Algorithm.Easy;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmTests.Easy
+{
+    public class MaximumSubarrayTests
+    {
+        private MaximumSubarray _member;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _member = new MaximumSubarray();
+        }
+
+        /// <summary>
+        /// 1 <= nums.length <= 10^5
+        /// -10^4 <= nums[i] <= 10^4
+        /// </summary>
+        [Test]
+        public void MaxSubArrayTest()
+        {
+            int[] nums1 = new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+            int[] nums2 = new int[] { 1 };
+            int[] nums3 = new int[] { 5, 4, -1, 7, 8 };
+            int[] nums4 = new int[] { -104, 104 };
+            int[] nums5 = new int[] { 0, 5, -1, -104, 56, -2, -2, -2, 104, -104, -100 };
+            int[] nums6 = new int[] { -2, -2, -1 };
+            int[] nums7 = new int[] { 5, 9, 4 };
+            int expected1 = 6;
+            int expected2 = 1;
+            int expected3 = 23;
+            int expected4 = 104;
+            int expected5 = 154;
+            int expected6 = -1;
+            int expected7 = 18;
+            Assert.AreEqual(expected1, _member.MaxSubArray(nums1));
+            Assert.AreEqual(expected2, _member.MaxSubArray(nums2));
+            Assert.AreEqual(expected3, _member.MaxSubArray(nums3));
+            Assert.AreEqual(expected4, _member.MaxSubArray(nums4));
+            Assert.AreEqual(expected5, _member.MaxSubArray(nums5));
+            Assert.AreEqual(expected6, _member.MaxSubArray(nums6));
+            Assert.AreEqual(expected7, _member.MaxSubArray(nums7));
+        }
+    }
+}
