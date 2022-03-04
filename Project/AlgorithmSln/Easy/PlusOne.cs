bf3e210a2105c923546a84698038c29902d644a1
Chengming Ruan (WICRESOFT NORTH AMERICA LTD) <v-cruan@microsoft.com>
2022-03-04 15:07:32 +0800

    66. Plus One (Easy)
    132 ms 44.46%

---
+++ Project/AlgorithmSln/Easy/PlusOne.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.Easy
+{
+    public class PlusOne
+    {
+        public int[] Plus(int[] digits)
+        {
+            if (digits.Length == 0)
+                throw new ArgumentNullException("Digits cannot be null or empty.");
+
+            int last = digits.Length - 1;
+            if (digits[last] != 9)
+            {
+                digits[last] = digits[last] + 1;
+                return digits;
+            }
+            while ( last >= 1&& digits[--last] == 9) ;
+            if (last == 0 && digits[last] == 9)
+            {
+                int[] result = new int[digits.Length + 1];
+                result[0] = 1;
+                return result;
+            }
+            else
+            {
+                for (int i = last+1; i < digits.Length; i++)
+                {
+                    digits[i] = 0;
+                }
+                digits[last] = digits[last] + 1;
+                return digits;
+            }
+        }
+    }
+}
---
+++ Project/Tests/Easy/PlusOneTests.cs
@@ -0,0 +1,52 @@
+using Algorithm.Easy;
+using Common;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlorithmTests.Easy
+{
+    public class PlusOneTests
+    {
+        private PlusOne _member;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _member = new PlusOne();
+        }
+
+        /// <summary>
+        /// 1 <= digits.length <= 100
+        /// 0 <= digits[i] <= 9
+        /// digits does not contain any leading 0's.
+        /// <summary>
+        [Test]
+        public void Plus()
+        {
+            int[] digits1 = new int[] { 1, 2, 3 };
+            int[] digits2 = new int[] { 4,3,2,1 };
+            int[] digits3 = new int[] { 9 };
+            int[] digits4 = new int[] { 9, 9, 9, 9, 9 };
+            int[] digits5 = new int[] { 8, 9,9,9,9 };
+            int[] digits6 = new int[] { 1,5,3,9,5,9 };
+            int[] digits7 = new int[] { 1 };
+            int[] expected1 = new int[] { 1, 2, 4 };
+            int[] expected2 = new int[] { 4,3,2,2 };
+            int[] expected3 = new int[] { 1, 0 };
+            int[] expected4 = new int[] { 1, 0,0,0,0,0 };
+            int[] expected5 = new int[] { 9,0,0,0,0 };
+            int[] expected6 = new int[] { 1,5,3,9,6,0 };
+            int[] expected7 = new int[] {  2 };
+            Assert.AreEqual(MyFormat.Convert(expected1), MyFormat.Convert(_member.Plus(digits1)));
+            Assert.AreEqual(MyFormat.Convert(expected2), MyFormat.Convert(_member.Plus(digits2)));
+            Assert.AreEqual(MyFormat.Convert(expected3), MyFormat.Convert(_member.Plus(digits3)));
+            Assert.AreEqual(MyFormat.Convert(expected4), MyFormat.Convert(_member.Plus(digits4)));
+            Assert.AreEqual(MyFormat.Convert(expected5), MyFormat.Convert(_member.Plus(digits5)));
+            Assert.AreEqual(MyFormat.Convert(expected6), MyFormat.Convert(_member.Plus(digits6)));
+            Assert.AreEqual(MyFormat.Convert(expected7), MyFormat.Convert(_member.Plus(digits7)));
+        }
+    }
+}
+
