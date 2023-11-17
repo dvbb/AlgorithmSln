1834ed78e68c14ba3eb9a17c5c8fd3d5413ba8b1
Chengming <v-cruan@microsoft.com>
2023-11-17 10:41:54 +0800

    67. Add Binary
    72ms    defeat 84.58%;  39.41MB defeat 31.34%

---
+++ Project/AlgorithmSln/Easy/AddBinary.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.Easy
+{
+    public class AddBinary
+    {
+        /// <summary>
+        /// 72ms    defeat 84.58%
+        /// 39.41MB defeat 31.34%
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public string AddBinaryMethod(string a, string b)
+        {
+            if (a == "")
+                return b;
+            if (b == "")
+                return a;
+            int i = a.Length - 1;
+            int j = b.Length - 1;
+            int carry = 0;
+            string result = "";
+            while (i >= 0 || j >= 0 || carry > 0)
+            {
+                int digit1 = i >= 0 ? a[i] - '0' : 0;
+                int digit2 = j >= 0 ? b[j] - '0' : 0;
+                int sum = digit1 + digit2 + carry;
+                carry = sum >= 2 ? 1 : 0;
+                result = (sum % 2).ToString() + result;
+                i--;
+                j--;
+            }
+            return result;
+        }
+    }
+}
---
+++ Project/Tests/Easy/AddBinaryTests.cs
@@ -0,0 +1,49 @@
+﻿using Algorithm.Easy;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmTests.Easy
+{
+    internal class AddBinaryTests
+    {
+        private AddBinary _member;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _member = new AddBinary();
+        }
+
+        /// <summary>
+        /// 1 <= a.length, b.length <= 104
+        /// a and b consist only of '0' or '1' characters.
+        /// Each string does not contain leading zeros except for the zero itself.
+        /// </summary>
+        [Test]
+        public void strStr()
+        {
+            string a1 = "11"; string b1 = "1";
+            string expected1 = "100";
+
+            string a2 = "1010"; string b2 = "1011";
+            string expected2 = "10101";
+
+            string a3 = "0"; string b3 = "0";
+            string expected3 = "0";
+
+            string a4 = "111"; string b4 = "0";
+            string expected4 = "111";
+
+            string a5 = "0"; string b5 = "111";
+            string expected5 = "111";
+
+            Assert.AreEqual(expected1, _member.AddBinaryMethod(a1, b1));
+            Assert.AreEqual(expected2, _member.AddBinaryMethod(a2, b2));
+            Assert.AreEqual(expected3, _member.AddBinaryMethod(a3, b3));
+            Assert.AreEqual(expected4, _member.AddBinaryMethod(a4, b4));
+            Assert.AreEqual(expected5, _member.AddBinaryMethod(a5, b5));
+        }
+    }
+}
